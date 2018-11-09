cb7159992bdc5bad3bf007928a856185d1808613
XYmar <xqingliuwu@163.com>
2018-11-09 13:07:42 +0800

    添加普通用户看板页面，实现看板查询功能

--- src/router/index.js
+++ src/router/index.js
@@ -35,6 +35,8 @@ export const constantRouterMap = [
   { path: '/authredirect', component: _import('login/authredirect'), hidden: true },
   { path: '/404', component: _import('errorPage/404'), hidden: true },
   { path: '/401', component: _import('errorPage/401'), hidden: true },
+  { path: '/userBoard', component: _import('userBoard/userBoard'), hidden: true },
+
   {
     path: '',
     component: Layout,
