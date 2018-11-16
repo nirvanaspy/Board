db4e975aeced3da626e2a75910d06a1b40db52aa
XYmar <xqingliuwu@163.com>
2018-11-16 09:54:25 +0800

    隐藏全屏的消息滚动页面路由

--- src/router/index.js
+++ src/router/index.js
@@ -418,12 +418,12 @@ export const asyncRouterMap = [
     path: '/plantTitle2',
     component: Layout,
     children: [{ path: 'index', component: _import('caption/plantTitle2'), name: 'plantTitle2', meta: { title: '标题2', icon: 'components1' }}]
-  },*/
+  },
   {
     path: '/messageAll',
     component: Layout,
     children: [{ path: 'index', component: _import('caption/messageAll'), name: 'messageAll', meta: { title: '标题3', icon: 'components1' }}]
-  },
+  },*/
   {
     path: '/gridLayout',
     component: Layout,
