cae49ca67cdfd8cdb78f064fedb75a965d99041e
nirvana <nirvana_spy@outlook.com>
2018-11-20 17:01:16 +0800

    删除非必要文件及代码；

--- src/api/deployPlanDetail.js
+++
@@ -1,8 +0,0 @@
-import request from '../utils/request'
-
-export function bindDetailList(deployplanId) {
-  return request({
-    url: 'deploymentdesigns/' + deployplanId + '/deploymentdesigndetails',
-    method: 'get'
-  })
-}
